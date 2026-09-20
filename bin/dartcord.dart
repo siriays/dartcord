00321c0e90eadff7d0ffeb7bc27e6684f04806cd
Yasmim <yasmimsiria09@gmail.com>
2026-09-20 00:12:08 -0400

    Finaliza arquivo principal do DartCord

--- bin/dartcord.dart
+++ bin/dartcord.dart
@@ -1,5 +1,49 @@
-import 'package:dartcord/dartcord.dart' as dartcord;
+import 'package:dartcord/models/usuario.dart';
+import 'package:dartcord/models/moderador.dart';
+import 'package:dartcord/models/canal.dart';
+import 'package:dartcord/models/mensagem.dart';
+import 'package:dartcord/models/servidor.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dartcord.calculate()}!');
+void main() {
+  final yasmim = Usuario(
+    nome: 'Yasmim Síria',
+    apelido: 'Yasmim',
+    online: true,
+  );
+
+  final lucas = Moderador(
+    nome: 'Lucas',
+    apelido: 'Luke',
+    online: true,
+  );
+
+  final geral = Canal(nome: 'geral');
+  final dart = Canal(nome: 'dart');
+  final flutter = Canal(nome: 'flutter');
+
+  final mensagem1 = Mensagem(
+    autor: yasmim,
+    texto: 'Olá, pessoal!',
+  );
+
+  final mensagem2 = Mensagem(
+    autor: lucas,
+    texto: 'Bem-vindos ao DartCord!',
+  );
+
+  dart.adicionarMensagem(mensagem1);
+  dart.adicionarMensagem(mensagem2);
+
+  final servidor = Servidor(
+    nome: 'Programação Mobile',
+  );
+
+  servidor.adicionarUsuario(yasmim);
+  servidor.adicionarUsuario(lucas);
+
+  servidor.adicionarCanal(geral);
+  servidor.adicionarCanal(dart);
+  servidor.adicionarCanal(flutter);
+
+  servidor.gerarRelatorio('dart');
 }
